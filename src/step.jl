94fa9ba4ff2989e956bdf9d8fa5932c1228187eb
Jonathan <jmiller@dundee.ac.uk>
2021-07-20 19:42:14 +0100

    Adding stepper function

--- src/step.jl
+++ src/step.jl
@@ -16,3 +16,15 @@ end
 Base.:+(w::Walker2D, s::Step2D) = Walker2D(w.x+s.x,w.y+s.y)
 
 Base.:+(s₁::Step2D,s₂::Step2D) = Walker2D(s₁.x+s₂.x,s₁.y+s₂.y)
+
+
+
+function stepper(x::Int64,r::Float64) 
+    if x == 0
+        return stepping(r)
+    elseif x == 1
+        return stepping(0.0)
+    else
+        error("input meant to be only 0 or 1 not $(x)")
+    end
+end
